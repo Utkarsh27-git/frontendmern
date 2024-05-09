0fd4bf5c1ed360726f09c33eed7173f61f8c388b
iutkarsh27-solanki <111502301+Utkarsh27-git@users.noreply.github.com>
2024-05-10 00:32:07 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './index.css';
-import { BrowserRouter as Router, Route } from "react-router-dom"; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/navbar.component";
 import ExerciseList from './components/exercises-list.component'
 import EditExercise from './components/edit-exercises.component'
 import CreateExercise from './components/create-exercises.component'
 import CreateUser from './components/create-user.component'
-
-const {REACT_APP_PORT} = process.env;
-
 
 
 function App() {
@@ -18,10 +15,12 @@ function App() {
 		<div className="container">
 		<Navbar />
 		<br/>
+		<Routes>
 		<Route path="/" exact component={ExerciseList} />	  
 		<Route path="/edit/:id" component={EditExercise} />	  
 		<Route path="/create" component={CreateExercise} />	  
 		<Route path="/user" component={CreateUser} />
+		</Routes>
 		</div>	  
 	</Router>
   );
